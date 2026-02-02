13d0a8fb0bdfe8a54de7697d55017eee521f1c33
Hana Mohamed <elnegeryh@gmail.com>
2026-02-02 13:17:04 +0200

    feat: Add application routing configuration.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Notfound } from './notfound/notfound';
+import { RouterModule } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'home', component: Home, title: 'عدسة - الرئيسية' },
   { path: '**', component: Notfound, title: 'عدسة - الصفحة غير موجودة' },
 ];
+
+RouterModule.forRoot(routes, { useHash: true })
+  
